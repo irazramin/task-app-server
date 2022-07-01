49ea6bb66f58c73944a57df05361194679245907
irazramin <irazramin@gmail.com>
2022-07-01 10:15:04 +0600

    delete api added

--- index.js
+++ index.js
@@ -59,6 +59,13 @@ async function run() {
 
             const result  = await taskCollections.updateOne(filter,updatedDoc,option);
             res.send(result);
+        });
+
+        app.delete('task/:id', async (req,res) =>{
+            const id = req.params.id;
+            const filter = {_id:ObjectId(id)};
+            const result = await taskCollections.deleteOne(filter);
+            res.send(result);
         })
     } finally {
     }
